7db46c8e008f81de90d81abaa5e3fea513f88f17
adunolami <adunolaadenike20@gmail.com>
2023-11-27 15:12:07 -0800

    third:commit

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-function restart(){
-    location.reload
+function reset(){
+    location.reload()
 }
+
+var first =1
+function clicked(event){
+    if (first== 1){
+    event.srcElement.innerText = "X"
+    first = 2
+}
+else if (first== 2){
+    
+    event.srcElement.innerText = "O"
+    first = 1
+}
+}
+
+var a = document.getElementById("1st").innerHTML
+var b = document.getElementById("2nd").innerHTML
+var c = document.getElementById("3rd").innerHTML
+var d = document.getElementById("4th").innerHTML
+var e = document.getElementById("5th").innerHTML
+var f = document.getElementById("6th").innerHTML
+var g = document.getElementById("7th").innerHTML
+var h = document.getElementById("8th").innerHTML
+var i = document.getElementById("9th").innerHTML
